ed874132326a83c1b0431afc6ef2ea5b283410c3
Nilesh Kumar <nilesh.guptain@gmail.com>
2025-06-29 02:52:56 +0530

    Updated instrument.js

---
+++ frontend/instrument.js
@@ -0,0 +1,12 @@
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://33881920c473b9003c975ed7a4cdbbe2@o4509549105512448.ingest.de.sentry.io/4509578391191632",
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true
+});
+
+const container = document.getElementById(“app”);
+const root = createRoot(container);
+root.render(<App />);
