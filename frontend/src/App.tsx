a5f2bfc9b584de3874eafda44f1085a6ec7866b4
Nilesh Kumar <nilesh.guptain@gmail.com>
2025-07-01 01:04:44 +0530

    Add BoltBadge to App Component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import BoltBadge from "./components/BoltBadge";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import Leaderboard from "./pages/Leaderboard";
@@ -18,6 +19,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
+      <BoltBadge />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
