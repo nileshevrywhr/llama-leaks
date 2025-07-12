9ed8219256b6621278371d326564d4bb6692f6a9
Nilesh Kumar <nilesh.guptain@gmail.com>
2025-07-13 01:52:17 +0530

    Updated instrument.js

--- frontend/instrument.js
+++ frontend/instrument.js
@@ -2,6 +2,7 @@ import * as Sentry from "@sentry/react";
 
 Sentry.init({
   dsn: "https://33881920c473b9003c975ed7a4cdbbe2@o4509549105512448.ingest.de.sentry.io/4509578391191632",
+  tunnel: "/tunnel",
 
   // Adds request headers and IP for users, for more info visit:
   // https://docs.sentry.io/platforms/javascript/guides/react/configuration/options/#sendDefaultPii
